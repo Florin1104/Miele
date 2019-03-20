02db85bb609340291d7c4fd80219b98262ebdc82
Fabian Florin Vlasceanu <fabian.vlasceanu@miele.com>
2019-03-20 12:11:49 +0200

    Commit main example

--- WM_main/WM_main.ino
+++ WM_main/WM_main.ino
@@ -1,8 +1,20 @@
 
+#include <LiquidCrystal_I2C.h>
+#include <DHT_U.h>
+#include <DHT.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
 #define LED_WASHING_STARTED             ()
 
 #include "TwinDos.h";
+#include "HM_HeaterModule.h"
+#include "ControlPanel.h"
+#include "MotorDriver.h"
+#include "Sound.h"
+
 TwinDos twinDos_o;
+HeaterModule heaterModule_o;
+MotorDriver motorDriver_o;
 /*******************************************************************************
 @Typedefinitions (module-local)
 *******************************************************************************/
@@ -18,26 +30,68 @@ TwinDos twinDos_o;
 --------------------------------------------------------------------------------
 @Parameters    none
 *******************************************************************************/
+static uint8_t lastButtonPressed_8;
+
+ControlPanel controlPanel_o;
+
 void setup()
 {
     // put your setup code here, to run once:
-    Serial.begin(9600);
-    delay(1000);
 
-    twinDos_o.Initialise_e(18); //setup TwinDos pin number
+    Serial.begin(9600);                 // serial init for test messages
+    delay(1000);                        // give time to Wemos Lolin32 to finish setup
+    controlPanel_o.Initialise_e();      //be sure to initialise "s_pinLocation_au8[]" with propper pins
+    heaterModule_o.Initialise_u16(GPIO_NUM_25,GPIO_NUM_15);
+	motorDriver_o.Initialise_u16(GPIO_NUM_13, GPIO_NUM_12);
+
 }
 
 void loop()
 {
+	bool isPowerButtonPressed = false;
+	bool isStartButtonPressed = false;
+
+	
     // put your main code here, to run repeatedly:
-    if(twinDos_o.isPresent_b())
+    lastButtonPressed_8 = controlPanel_o.poolButtonsStateChanges_v(); // constantly check if a button was pressed
+    switch(lastButtonPressed_8)
     {
-        Serial.println("Connected");
+    case BUTTON_POWER_ID:
+        Serial.println("Power button pressed");
+		isPowerButtonPressed = true;
+
+        // TBD
+        break;
+    case BUTTON_START_STOP_ID:
+        Serial.println("Start/Stop button pressed");
+		isStartButtonPressed = true;
+
+    //    // TBD
+    //    break;
+    //case BUTTON_WASH_ID:
+    //    Serial.println("Wash button pressed");
+    //    // TBD
+    //    break;
+    //case BUTTON_SPIN_ID:
+    //    Serial.println("Spin button pressed");
+    //    // TBD
+    //    break;
+    //case BUTTON_DOOR_SWITCH_ID:
+    //    Serial.println("Door button pressed");
+    //    // TBD
+        break;
+    default:
+        break;
     }
-    else
-    {
-        Serial.println("NOT connected");
-    }
+
+	if(isPowerButtonPressed == true)
+	{
+		heaterModule_o.StartHeating_v(120);
+		float t= heaterModule_o.GetTemperature_f();
+		Serial.print(t);
+		motorDriver_o.MoveMotor_u16(50,MOTOR_ROTATION_CLOCKWISE,2);
+	}
+
 
 }
 
